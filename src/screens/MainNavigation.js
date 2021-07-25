ea653b66f6c826be906b29ebc4e377ce1d662ce7
haanh1602 <lean6801@gmail.com>
2021-07-25 22:45:12 +0700

    change MainNavigation.js

--- src/screens/MainNavigation.js
+++ src/screens/MainNavigation.js
@@ -1,19 +1,26 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Background from '../../src/data/backgrounds/Background';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button title="Detail" onPress={() => navigation.push('Detail')}></Button>
     </View>
   );
 }
 
-function DetailScreen() {
-  return <Background />;
+function DetailScreen({navigation}) {
+  return (
+    <Background>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button title="Detail1" onPress={() => navigation.push('Detail1')} />
+      {/* <Button title="Home" onPress={() => navigation.navigate('Home')}></Button> */}
+    </Background>
+  );
 }
 
 const Stack = createStackNavigator();
@@ -21,13 +28,14 @@ const Stack = createStackNavigator();
 function MainNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{title: 'Overview'}}
         />
         <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="Detail1" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
